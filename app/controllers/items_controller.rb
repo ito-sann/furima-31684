6a0da9b2710bba8d292063fc9da2ca93af8fb026
ito-sann <n87.ito.yutaro@gmail.com>
2020-11-16 16:28:23 +0900

    ひ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,17 +19,35 @@ class ItemsController < ApplicationController
     end
   end
 
+  # def show
+  #   @item = Item.find(params[:id])
+  # end
+
+  # def edit
+  #   @item = Item.find(params[:id])
+  # end
+
+  # def update
+  #   @item = Item.find(params[:id])
+  #   if @item.update(item_params)
+  #     redirect_to item_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :text, :price, :image, :burden_id, :category_id, :condition_id, :day_id, :region_id).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  # def set_item
+  #   @item = Item.find(params[:id])
+  # end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
-  end
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
+  # end
 end
