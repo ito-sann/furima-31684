fb3118346ab0c6f828a151f28a93f0a87be04732
ito-sann <n87.ito.yutaro@gmail.com>
2020-11-16 17:16:39 +0900

    編集機能のコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_item, only: [:edit, :show]
-  before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:show]
+  # before_action :move_to_index, only: [:edit]
   def index
     @items = Item.all.order(id: 'DESC')
   end
@@ -23,18 +23,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
    end
 
-   def edit
-    @item = Item.find(params[:id])
-  end
+  #  def edit
+  #   @item = Item.find(params[:id])
+  # end
 
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @item = Item.find(params[:id])
+  #   if @item.update(item_params)
+  #     redirect_to item_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
 
   private
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
-  end
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in? && current_user.id == @item.user.id
+  # end
 end
